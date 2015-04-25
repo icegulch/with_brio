ac5d97710950938bfca3ef990924cf3820d72277
icegulch <icegulch@gmail.com>
2015-04-25 08:08:06 -0700

    checkpoint

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,2 +1,11 @@
 //= require "jquery-1.11.1"
 //= require "waypoints"
+
+    $(function() {
+      // waypoints
+      jQuery('.page-section-work').waypoint(function() {
+        jQuery('.bobbles').toggleClass('animated bounce');
+      }, { offset: '70%' });
+
+    });
+
